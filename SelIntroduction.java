b4c756ac8781ccecbb20c1d3ecfbed065f227df4
Joe Duong <chau.duong1995@gmail.com>
2023-11-05 23:21:33 -0800

    quick_update

--- SelIntroduction.java
+++ SelIntroduction.java
@@ -3,7 +3,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SelIntroduction {
     public static void main(String[] args) {
-        System.out.println("Hello World"); 
         //Invoking Browers
         //Chrome Driver 
         System.setProperty("webdriver.chrome.driver", "/chromedriver_mac64/chromedriver");
